cdc9c55e27ecbdf1e9cefc878fd7e4d325923838
felipedba <felipedba2020@gmail.com>
2022-07-11 15:33:20 -0300

    Projeto Criado

---
+++ backend/src/main/java/com/devsuperior/dsmesta/DsmestaApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmesta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsmestaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsmestaApplication.class, args);
+	}
+
+}
